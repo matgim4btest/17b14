50198fe8188418761170f90a4f6b59dbe91e38a2
IdeGasMateja <mateja.maricic.mata@gmail.com>
2021-04-13 20:44:18 +0200

    skrtskrt1

--- src/main/java/rs/edu/matgim/zadatak/DB.java
+++ src/main/java/rs/edu/matgim/zadatak/DB.java
@@ -1,11 +1,16 @@
 package rs.edu.matgim.zadatak;
-
+//skrtskrt
 import java.sql.*;
 
 public class DB {
 
+    Connection connection;
     String connectionString = "jdbc:sqlite:src\\main\\java\\Banka.db";
 
+    public void connect() throws SQLException
+    {
+        this.connection=DriverManager.getConnection(connectionString);
+    }
     public void printFilijala() {
         try ( Connection conn = DriverManager.getConnection(connectionString);  Statement s = conn.createStatement()) {
 
@@ -22,6 +27,56 @@ public class DB {
             System.out.println("Greska prilikom povezivanja na bazu");
             System.out.println(ex);
         }
+    }
+    public void printePositiveRacun() throws SQLException
+    {
+        String upit="SELECT * FROM Racun WHERE Stanje>0";
+                Statement st=connection.createStatement();
+                ResultSet rs=st.executeQuery(upit);
+                while(rs.next())
+                {
+                    int a,b,c,d,e;
+                    a=rs.getInt(1);
+                    String s=rs.getString(2);
+                    b=rs.getInt(3);
+                    c=rs.getInt(4);
+                    d=rs.getInt(5);
+                    e=rs.getInt(6);
+                    System.out.println(a+"\t"+b+"\t"+s+"\t"+c+"\t"+d+"\t"+e+"\t");
+                }
+    }
+    boolean zadatak(int idRacFrom,int idRacTo, float c) throws SQLException
+    {
+        try{
+        connection.setAutoCommit(false);
+        String upit1="SELECT Stanje FROM Racun WHERE IdRac=?";
+        PreparedStatement st=connection.prepareStatement(upit);
+        st.setInt(1,idRacFrom);
+        ResultSet rs=st.executeQuery(upit);
+        connection.commit();
+        float stanjee=rs.getInt(1);
+        if(stanjee>c)
+        {
+            String upit2="UPDATE Racun SET Stanje=Stanje-? WHERE IdRac=?";
+            PreparedStatement st1=connection.prepareStatement(upit2);
+            st1.setFloat(1, c);
+            st1.setInt(2, idRacFrom);
+            st1.executeUpdate();
+            String upit3="UPDATE Racun SET Stanje=Stanje+? WHERE IdRac=?";
+            PreparedStatement st2=connection.prepareStatement(upit2);
+            st1.setFloat(1, c);
+            st1.setInt(2, idRacTo);
+            st2.executeUpdate();
+            connection.commit();
+        }
+        connection.setAutoCommit(true);
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("Dogodila se greska");
+            connection.rollback();
+        }
+        return true;
     }
 
 }
--- src/main/java/rs/edu/matgim/zadatak/Program.java
+++ src/main/java/rs/edu/matgim/zadatak/Program.java
@@ -1,11 +1,20 @@
 package rs.edu.matgim.zadatak;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Program {
 
     public static void main(String[] args) {
 
         DB _db = new DB();
         _db.printFilijala();
+        try {
+            _db.printePositiveRacun();
+        } catch (SQLException ex) {
+            System.out.println("Greska");
+        }
         
     }
 }
